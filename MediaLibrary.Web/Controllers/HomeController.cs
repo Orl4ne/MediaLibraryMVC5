d33ba16da120042c1da12c94470a4db407f58af0
Orlane Vermo <orlane_v@hotmail.com>
2020-06-15 14:24:10 +0200

    Add nuget Unity.MVC5 and dependancy injections

--- MediaLibrary.Web/Controllers/HomeController.cs
+++ MediaLibrary.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MediaLibrary.DAL;
+using MediaLibrary.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,8 +12,8 @@ namespace MediaLibrary.Web.Controllers
     {
         public ActionResult Index()
         {
-
-            return View();
+            
+            return View(MediaLibraryContext.Medias.ObtenirTousMedias());
         }
 
         public ActionResult About()
