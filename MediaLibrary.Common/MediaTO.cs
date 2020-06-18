1e47104a2503443b933a5547d4789accfc2b364c
Orlane Vermo <orlane_v@hotmail.com>
2020-06-18 22:16:22 +0200

    Adding validation

--- MediaLibrary.Common/MediaTO.cs
+++ MediaLibrary.Common/MediaTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,12 @@ namespace MediaLibrary.Common
     public class MediaTO
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(80, MinimumLength = 3)]
         public string Name { get; set; }
+        [Required]
         public string Path { get; set; }
+        [Required]
         public MediaType Type { get; set; }
     }
 }
---
+++ MediaLibrary.DAL/Migrations/202006182012464_v4.cs
@@ -0,0 +1,20 @@
+﻿namespace MediaLibrary.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v4 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Medias", "Name", c => c.String(nullable: false, maxLength: 80));
+            AlterColumn("dbo.Medias", "Path", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Medias", "Path", c => c.String());
+            AlterColumn("dbo.Medias", "Name", c => c.String());
+        }
+    }
+}
